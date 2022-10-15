d9c9486b60ea157f1421fffd98cad301f4ad37de
sakhilegoodwillshabangu <75811647+sakhilegoodwillshabangu@users.noreply.github.com>
2022-10-15 11:33:01 +0200

    my portfolio

---
+++ portfolio/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request, "portfolio/home.html")
+def projects(request):
+    return render(request, "portfolio/projects.html")
+def about(request):
+    return render(request, "portfolio/about.html")
